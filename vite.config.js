06e0ae0d8f6d662590e78d537f8c58316ccedbb5
FedericaCampo97 <federica.campo97@gmail.com>
2023-11-09 00:37:27 +0100

    separate the controllers from the admin area

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            // edit the first value of the array input to point to our new sass files and folder.
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    // Add resolve object and aliases
+    resolve: {
+        alias: {
+            '~bootstrap': '/node_modules/bootstrap',
+            '~resources': '/resources/'
+        }
+    }
 });
