7242963779158a360a8a3b54198392c0a97a7511
FedericaCampo97 <federica.campo97@gmail.com>
2023-11-16 14:36:56 +0100

    Add style dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,17 +4,17 @@
 
 <body class="bg-img">
     <div class="container">
-        <h2 class="text-secondary my-5 text-center text-white">
+        <h1 class="text-secondary mt-5 title_welcome text-center text-white">
             {{ __('Welcome') }} {{Auth::user()->name}}!
-        </h2>
+        </h1>
         @if (session('status'))
         <div class="alert alert-success" role="alert">
             {{session('status')}}
         </div>
         @endif
-        <div class="row justify-content-center">
+        <div class="row justify-content-center block_card">
             <div class="col">
-                <div class="card">
+                <div class="card demo">
                     <div class="card-body">
                         <h3>Projects</h3>
                         <strong class="fs-3">{{$total_project}}</strong>
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card demo">
                     <div class="card-body">
                         <h3>User</h3>
                         <strong class="fs-3">{{$total_users}}</strong>
@@ -30,7 +30,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card demo">
                     <div class="card-body">
                         <h3>Technologies</h3>
                         <strong class="fs-3">{{$total_technologies}}</strong>
